5b38cb33713fca187c2df7b9cfc761119115107f
Lu Murga <lulipsicounc@gmail.com>
2021-10-10 15:36:18 -0300

    Creación de la interface IEstudioComplementario en el Package repository.impl

---
+++ proyecto_final_back/src/main/java/com/backend/proyectofinal/repository/impl/IEstudioComplementarioRepository.java
@@ -0,0 +1,20 @@
+package com.backend.proyectofinal.repository.impl;
+
+import com.backend.proyectofinal.entity.EstudioComplementario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IEstudioComplementarioRepository extends JpaRepository<EstudioComplementario, Long> {
+
+    @Query("SELECT e FROM Estudio e WHERE e.paciente.dni = ?1")
+    List<EstudioComplementario> findEstudiosByPacienteDni(Integer dni);
+
+    @Query("SELECT e FROM Estudio e WHERE e.odontologo.matricula  = ?1")
+    List<EstudioComplementario> findEstudiosByOdontologoMatricula(String matricula);
+
+    @Query("SELECT e FROM Estudio e WHERE e.fecha = ?1")
+    List<EstudioComplementario> findEstudioByFecha(LocalDate fecha);
+}
